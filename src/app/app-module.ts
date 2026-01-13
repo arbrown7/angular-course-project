d786232c111f5f60ee6dd04bc0c1b6f6d39add61
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-12 21:43:17 -0700

    Creating and using the components
    Course Project - The Basics

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,10 +2,24 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { Header } from './header/header';
+import { Recipes } from './recipes/recipes';
+import { RecipeList } from './recipes/recipe-list/recipe-list';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail';
+import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item';
+import { ShoppingList } from './shopping-list/shopping-list';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Recipes,
+    RecipeList,
+    RecipeDetail,
+    RecipeItem,
+    ShoppingList,
+    ShoppingEdit
   ],
   imports: [
     BrowserModule
