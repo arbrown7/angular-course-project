cc77284b35be11ab14579028db7d7412a43ebc3d
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-20 10:12:24 -0700

    Allowing the user to add ingredients to the shopping list
    Course Project - Components and Databinding

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,5 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { App } from './app';
 import { Header } from './header/header';
@@ -22,7 +23,8 @@ import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit';
     ShoppingEdit
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
