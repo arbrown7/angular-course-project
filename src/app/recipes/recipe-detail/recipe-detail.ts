b4bfc8bd8ec02837ca39a46f5753088aaa393665
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-02 21:36:24 -0700

    passing ingredients from recipes to the shopping list (via a service)
    Services and Dependency Injection

--- src/app/recipes/recipe-detail/recipe-detail.ts
+++ src/app/recipes/recipe-detail/recipe-detail.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,7 +8,18 @@ import { RecipeModel } from '../recipe.model';
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
 })
-export class RecipeDetail {
+export class RecipeDetail implements OnInit {
   @Input() recipe: RecipeModel;
 
+  constructor(private recipeService: RecipeService) {
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,34 +1,44 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { RecipeModel } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService  {
     recipeSelected = new EventEmitter<RecipeModel>();
 
     private recipes: RecipeModel[] = [
-    new RecipeModel(
-        'Acai Bowl: Planet Bowl', 
-        'Very environmental', 
-        'https://hlyfemethod.com/wp-content/uploads/2018/04/rainbow-bowl-photo.jpg',
-        [
-            new Ingredient('strawberries', 12),
-            new Ingredient('acai', 1)
-        ]
-    ),
-    new RecipeModel(
-        'Acai Bowl: Twinkling Purple Fancy Bowl', 
-        'I think the name is funny', 
-        'https://hlyfemethod.com/wp-content/uploads/2018/04/rainbow-bowl-photo.jpg',
-        [
-            new Ingredient('star fruit', 1),
-            new Ingredient('acai', 1),
-            new Ingredient('yogurt', 1)
-        ]
+        new RecipeModel(
+            'Acai Bowl: Planet Bowl', 
+            'Very environmental', 
+            'https://hlyfemethod.com/wp-content/uploads/2018/04/rainbow-bowl-photo.jpg',
+            [
+                new Ingredient('strawberries', 12),
+                new Ingredient('acai', 1)
+            ]
+        ),
+        new RecipeModel(
+            'Acai Bowl: Twinkling Purple Fancy Bowl', 
+            'I think the name is funny', 
+            'https://hlyfemethod.com/wp-content/uploads/2018/04/rainbow-bowl-photo.jpg',
+            [
+                new Ingredient('star fruit', 1),
+                new Ingredient('acai', 1),
+                new Ingredient('yogurt', 1)
+            ]
     )
     ];
 
+    constructor(private slService: ShoppingListService) {
+
+    }
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 
 }
